a9510f22694b0d497ac47d4fc7d186c37e996608
udinesia325 <udinesia325@gmail.com>
2022-10-27 20:03:12 +0700

    menambahkan express validator + slugify dan menambahkan express.json di app

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
-const Express = require('express')
+const Express = require("express")
 
 const app = new Express()
-app.use(Express.static(__dirname + '/public'));
-app.use((req,res,next) => {
-		res.setHeader("Access-Control-Allow-Origin","*")
-		next()
-		
+app.use(Express.static(__dirname + "/public"))
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    next()
 })
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }))
 exports.app = app
