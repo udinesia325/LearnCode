dd10e74c3288e7f01acc169432e35043827e6082
udinesia325 <udinesia325@gmail.com>
2022-10-30 17:10:17 +0700

    memperbaiki struktur field karena perubahan tabel

--- backend/app/database/models/users.js
+++ backend/app/database/models/users.js
@@ -1,30 +1,32 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-const {timestamps} = require('helpers/underscore_timestamp');
+"use strict"
+const { Model } = require("sequelize")
+const { timestamps } = require("helpers/underscore_timestamp")
 module.exports = (sequelize, DataTypes) => {
-  class users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class users extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  users.init({
-    uid: DataTypes.STRING,
-    name: DataTypes.STRING,
-    photo: DataTypes.STRING,
-    email: DataTypes.STRING,
-    email_verified_at: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'users',
-    underscored: true,
-			...timestamps
-  });
-  return users;
-};
+    users.init(
+        {
+            uid: DataTypes.STRING,
+            name: DataTypes.STRING,
+            password: DataTypes.STRING,
+            photo: DataTypes.STRING,
+            email: DataTypes.STRING,
+            email_verified_at: DataTypes.DATE,
+        },
+        {
+            sequelize,
+            modelName: "users",
+            underscored: true,
+            ...timestamps,
+        }
+    )
+    return users
+}
