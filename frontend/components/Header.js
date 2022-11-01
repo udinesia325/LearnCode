664461b5de3ef04aa9de40b67c3ead67cd506ea7
udinesia325 <udinesia325@gmail.com>
2022-11-01 15:49:30 +0700

    membuat component menjadi bootstrap

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,126 +1,29 @@
-import Image from "next/image";
-import Link from "next/link";
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
 
-export default function Header() {
+function Header({ show, setShow }) {
+    const handleShow = (e) => {
+        e.preventDefault()
+        setShow(true)
+    }
+
     return (
-        <div
-            id="header"
-            align="center"
-            style={{
-                paddingTop: "100px",
-                paddingBottom: "100px",
-            }}
-        >
-            <p align="center" style={{ margin: "0" }}>
-            </p>
-            <h1
-                id="main_title"
-                style={{
-                    textShadow: " 3px 3px 1px rgb(215, 215, 215)",
-                    margin: "2px",
-                }}
-            >
-                Learn Code - Your Guide Of Programming
-            </h1>
-            <a
-                href="index.html"
-                className="btn"
-                style={{
-                    background: "#3479AB",
-                }}
-                onmouseover="btn_title_show('Main page of this website') "
-                onmouseout="btn_title_show('')"
-            >
-                <span
-                    style={{
-                        fontSize: "50px",
-                    }}
-                    className="fa fa-home"
-                ></span>
-                <br />
-                HOME
-            </a>
-						<Link href={"/lesson"}>
-            <a
-                
-                className="btn"
-                style={{ background: "#4CAF50" }}
-                onmouseover="btn_title_show('Free Learn code')"
-                onmouseout="btn_title_show('')"
-            >
-                <span
-                    style={{
-                        fontSize: "50px",
-                    }}
-                    className="fa fa-rocket"
-                ></span>
-                <br />
-                LEARN
-            </a>
-								</Link>
-            <a
-                href="404.html"
-                className="btn"
-                style={{
-                    background: "#E91E63",
-                }}
-                onmouseover="btn_title_show('Free learn code')"
-                onmouseout="btn_title_show('')"
-            >
-                <span
-                    style={{
-                        fontSize: "50px",
-                    }}
-                    className="fa fa-gamepad"
-                ></span>
-                <br />
-                SAMPEL
-            </a>
-            <a
-                href="https://learncodeteam.blogspot.com"
-                className="btn"
-                style={{
-                    background: "#673AB7",
-                }}
-                onmouseover="btn_title_show('Last news about android and apps development')"
-                onmouseout="btn_title_show('')"
-            >
-                <span
-                    style={{
-                        fontSize: "50px",
-                    }}
-                    className="fa fa-newspaper-o"
-                ></span>
-                <br />
-                NEWS
-            </a>
-            <a
-                href="#"
-                className="btn"
-                style={{
-                    background: "#FF9800",
-                }}
-                onmouseover="btn_title_show('Some information about this website')"
-                onmouseout="btn_title_show('')"
-                onClick="about_dialog_show()"
-            >
-                <span
-                    style={{
-                        fontSize: "50px",
-                    }}
-                    className="fa fa-user"
-                ></span>
-                <br />
-                ABOUT
-            </a>
-            <p
-                id="btn_title"
-                style={{
-                    position: "absolute",
-                    left: "100px",
-                    right: "100px",
-                }}
-            ></p>
-        </div>
+        <Navbar variant="light" bg="light">
+            <Container>
+                <Navbar.Brand
+                    href="#"
+                    className="bx bx-menu"
+                    onClick={handleShow}
+                ></Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home" active>
+                        LearnCode
+                    </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
+
+export default Header
