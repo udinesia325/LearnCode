d41e8a9fa0afae3136d06af966bbf5101658f9d1
udinesia325 <udinesia325@gmail.com>
2022-12-17 17:05:35 +0700

    improve sidebar

--- frontend/components/Sidebar.js
+++ frontend/components/Sidebar.js
@@ -1,22 +1,46 @@
-import {Offcanvas} from "react-bootstrap"
+import { ListGroup, Offcanvas } from "react-bootstrap"
 import Link from "next/link"
-
+const dataList = [
+    {
+        href:"/",
+        icon:"fa-solid fa-house",
+        text:"Home"
+    },
+    {
+        href:"/lessons",
+        icon:"fa-solid fa-book",
+        text:"Lessons"
+    },
+    {
+        href:"/about",
+        icon:"fa-solid fa-newspaper-o",
+        text:"About"
+    }
+]
 function Sidebar({ show, setShow }) {
     const handleClose = () => setShow(false)
 
     return (
-        <Offcanvas show={show} onHide={handleClose} style={{maxWidth:"80vw"}}>
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>LearnCode</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <div clasName="flex-column text-dark">
-                        <Link href="/">
-                            <a>Home</a>
+        <Offcanvas show={show} onHide={handleClose} style={{ maxWidth: "80vw" }}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>LearnCode</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <ListGroup variant="flush">
+                    {
+                        dataList.map((data,index)=>(
+                    <ListGroup.Item key={index} className="d-flex">
+                        <Link href={data.href}>
+                            <a onClick={handleClose} className="text-dark fw-bold w-100"><i class={`me-4 ${data.icon}`}></i>{data.text}</a>
                         </Link>
-                    </div>
-                </Offcanvas.Body>
-            </Offcanvas>
+                    </ListGroup.Item>
+
+
+                        ))
+                    }
+                </ListGroup>
+            </Offcanvas.Body>
+        </Offcanvas>
     )
 }
 
